b0e00e37f4c2bd9ee440516ada05557f0a2581b4
Thomas Kjøglum <thomas@kjoglum.com>
2017-07-16 14:57:55 +0200

    Added sumOfEvenNumbers and Fibanocci functions

--- math.py
+++ math.py
@@ -9,6 +9,23 @@ def modulo5(n):
     if n % 5: return False
     else: return True
 
+def sumOfEvenNumbers(list):
+    sumOfEvenNumbers = 0
+    for n in list:
+        if not n % 2:
+            sumOfEvenNumbers = sumOfEvenNumbers + n
+    return sumOfEvenNumbers
+
+def fibonacci(highestFibonacciNumber):
+    i = 1
+    j = 2
+    fibNumbers = [i]
+    while j < highestFibonacciNumber:
+        fibNumbers.append(j)
+        k = i + j
+        i = j
+        j = k
+    return fibNumbers
 
 class Test(unittest.TestCase):
     def testModulo3(self):
@@ -23,3 +40,9 @@ class Test(unittest.TestCase):
         self.assertTrue(modulo5(1005))
         self.assertFalse(modulo5(111))
         self.assertFalse(modulo5(4))
+
+    def testFibonacci(self):
+        self.assertEqual(fibonacci(100),[1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
+
+    def testSumOfEvenNumbers(self):
+        self.assertEqual(sumOfEvenNumbers([1, 2, 3, 5, 8, 13, 21, 34, 55, 89]), 44)
